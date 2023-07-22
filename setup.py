2ca3a6a2be1e3ee45ad7095e9e9de4d746b93792
Silei <shuo.2020@outlook.com>
2023-07-21 21:00:39 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='easystockplot',
-    version='0.1',
+    version='0.1.1',
     description='an easy stockplot package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='S Huo',
     url='https://sl-huo.github.io/',
     packages=['easystockplot'],
     install_requires=['yfinance', 'matplotlib', 'seaborn'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    keywords=['python', 'stock', 'plot'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
